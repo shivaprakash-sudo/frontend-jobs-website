5a5db656214cc04112805a6e0207162e588f4f13
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2023-01-31 23:28:08 +0000

    add prettier config settings

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import fetch from "node-fetch";
 import cors from "cors";
 
 if (process.env.NODE_ENV !== "production") {
-  const { default: dotenv } = import("dotenv");
-  dotenv.config();
+    const { default: dotenv } = import("dotenv");
+    dotenv.config();
 }
 
 const app = express();
@@ -14,19 +14,19 @@ app.use(cors());
 const url = `https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.apiID}&app_key=${process.env.apiKey}`;
 
 app.get("/", async (req, res) => {
-  const data = await getData(url);
-  const results = await data.results;
-  console.log(results)
-  res.json(results);
+    const data = await getData(url);
+    const results = await data.results;
+    console.log(results);
+    res.json(results);
 });
 
 function getData(url) {
-  const data = fetch(url)
-    .then((response) => response.json())
-    .catch((err) => console.error(err));
-  return data;
+    const data = fetch(url)
+        .then((response) => response.json())
+        .catch((err) => console.error(err));
+    return data;
 }
 
 app.listen(3000, () => {
-  console.log(`Listening on PORT 3000`);
+    console.log(`Listening on PORT 3000`);
 });
